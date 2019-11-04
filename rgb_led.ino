9adaf56a3cc02eee2d824da22a3bcc763d6c4abb
Antonio Ćudina <57157876+acudina@users.noreply.github.com>
2019-11-04 20:44:20 +0100

    Add files via upload

---
+++ rgb_led.ino
@@ -0,0 +1,23 @@
+
+int red =3;
+int green =5;
+int blue =6;
+
+void setup() {
+  // put your setup code here, to run once
+  pinMode(3,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  
+  analogWrite(red,100);
+  analogWrite(blue,100);
+  analogWrite(green,100);
+  
+
+  
+}
